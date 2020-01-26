fb771da17dac03e0fdbbb352fda184e468f5f8f9
tinaavbelj <tina.avbelj@gmail.com>
2020-01-26 19:50:48 +0100

    Added slider

--- src/components/main/Filters/components/Stroke.js
+++ src/components/main/Filters/components/Stroke.js
@@ -1,35 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-import BoxShadow from "components/shared/BoxShadow";
-import Input from "components/shared/Input";
+import Slider from "@material-ui/core/Slider";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-const Stroke = ({ iconStroke, onIconStrokeChange }) => (
-  <Wrapper>
-    <Box>
-      <div>Stroke</div>
-      <StyledBoxShadow>
-        <Input
-          value={iconStroke}
-          onChange={event => onIconStrokeChange(event.target.value)}
+const Stroke = ({ iconStroke, onIconStrokeChange }) => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#000000"
+      }
+    }
+  });
+
+  return (
+    <Wrapper>
+      <Box>
+        <Heading>stroke width</Heading>
+        <div>{iconStroke}</div>
+      </Box>
+      <MuiThemeProvider theme={theme}>
+        <Slider
+          defaultValue={iconStroke}
+          min={1}
+          max={3}
+          step={0.5}
+          onChange={(event, value) => onIconStrokeChange(value)}
         />
-      </StyledBoxShadow>
-    </Box>
-  </Wrapper>
-);
+      </MuiThemeProvider>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div``;
+
+const Heading = styled.div`
+  margin: 0 0 8px 4px;
+  font-weight: bold;
+`;
 
 const Box = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-const StyledBoxShadow = styled(BoxShadow)`
-  padding: 0.5em;
-  border-radius: 8px;
-  width: 45px;
+  margin-bottom: 1rem;
 `;
 
 export default Stroke;
